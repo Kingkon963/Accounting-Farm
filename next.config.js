938268ff29821825e0fe459cf2c04a5b6180d3e2
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2020-11-19 13:25:48 +0600

    u

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     async headers() {
       return [
         {
-          source: '/:path*',
+          source: '/api/*',
           headers: [
             {
               key: 'Content-Type',
