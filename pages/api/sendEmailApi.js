1b540a057afa2b6fa32cbd841e55b8fe16e5d690
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2020-11-19 16:55:11 +0600

    Testing Email

--- pages/api/sendEmailApi.js
+++ pages/api/sendEmailApi.js
@@ -13,8 +13,8 @@ export default function handler(req, res) {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
       host: "smtp.ethereal.email",
-      port: 465,
-      secure: true, // true for 465, false for other ports
+      port: 587,
+      secure: false, // true for 465, false for other ports
       auth: {
         user: testAccount.user, // generated ethereal user
         pass: testAccount.pass, // generated ethereal password
